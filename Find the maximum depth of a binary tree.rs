776ddaeaf6678ffde2635d7e00f0c5822d4f12fe
glenngomey <153701628+glenngomey@users.noreply.github.com>
2024-04-22 20:03:23 +0530

    Create Find the maximum depth of a binary tree.rs

---
+++ Find the maximum depth of a binary tree.rs
@@ -0,0 +1,45 @@
+struct TreeNode {
+    val: i32,
+    left: Option<Box<TreeNode>>,
+    right: Option<Box<TreeNode>>,
+}
+
+
+fn max_depth(root: &Option<Box<TreeNode>>) -> i32 {
+    match root {
+        Some(node) => {
+            let left_depth = max_depth(&node.left);
+            let right_depth = max_depth(&node.right);
+            1 + left_depth.max(right_depth)
+        }
+        None => 0,
+    }
+}
+
+fn main() {
+  
+    let root = Some(Box::new(TreeNode {
+        val: 3,
+        left: Some(Box::new(TreeNode {
+            val: 9,
+            left: None,
+            right: None,
+        })),
+        right: Some(Box::new(TreeNode {
+            val: 20,
+            left: Some(Box::new(TreeNode {
+                val: 15,
+                left: None,
+                right: None,
+            })),
+            right: Some(Box::new(TreeNode {
+                val: 7,
+                left: None,
+                right: None,
+            })),
+        })),
+    }));
+
+    let depth = max_depth(&root);
+    println!("Maximum depth of the binary tree: {}", depth);
+}
